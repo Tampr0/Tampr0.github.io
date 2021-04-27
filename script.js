cfcd63ee1cd4a7c82c0f75d509e3e00d4b9b88b1
Tampr0 <31279986+Tampr0@users.noreply.github.com>
2021-04-27 17:44:28 +0200

    Url to restful service changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://powerful-wave-02152.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://powerful-wave-02152.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://suhe-task-manager.herokuapp.com/ /v1/task/';
+  const trelloApiRoot = 'https://suhe-task-manager.herokuapp.com/ /v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
